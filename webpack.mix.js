b2d8043f5c68295add74a4a2278fee3beb9ea4f2
Zeilar <philip@angelin.dev>
2021-01-23 14:27:59 +0100

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,3 +3,19 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .react()
     .sass('resources/sass/app.scss', 'public/css');
+
+if (mix.inProduction()) {
+	// App
+	mix.copyDirectory('app', 'dist/src/app');
+    mix.copyDirectory('bootstrap', 'dist/src/bootstrap');
+	mix.copyDirectory('config', 'dist/src/config');
+	mix.copyDirectory('database', 'dist/src/database');
+	mix.copyDirectory('resources', 'dist/src/resources');
+	mix.copyDirectory('routes', 'dist/src/routes');
+    // mix.copyDirectory('storage', 'dist/src/storage');
+	mix.copyDirectory('tests', 'dist/src/tests');
+
+	// Public
+	mix.copyDirectory('public/css', 'dist/public_html/css');
+	mix.copyDirectory('public/js', 'dist/public_html/js');
+}
